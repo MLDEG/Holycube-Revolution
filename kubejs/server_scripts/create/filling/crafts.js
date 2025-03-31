c757c6ce691686283263aaf365ef44cea7936d64
MLDEG <mrmldeg@gmail.com>
2025-03-31 19:13:16 +0200

    Finish the release 1.0.0

--- kubejs/server_scripts/create/filling/crafts.js
+++ kubejs/server_scripts/create/filling/crafts.js
@@ -1,14 +1,12 @@
 ServerEvents.recipes(e => {
-
-
     function dyeCrafts(dye) {
         e.custom({ "type": "create:filling", "ingredients": [{ "item": "minecraft:white_wool" }, { "type": "fluid_stack", "amount": 50, "fluid": "kubejs:" + dye }], "results": [{ "id": "minecraft:" + dye + "_wool" }] })
         e.custom({ "type": "create:filling", "ingredients": [{ "item": "minecraft:glass" }, { "type": "fluid_stack", "amount": 50, "fluid": "kubejs:" + dye }], "results": [{ "id": "minecraft:" + dye + "_stained_glass" }] })
         e.custom({ "type": "create:filling", "ingredients": [{ "item": "minecraft:terracotta" }, { "type": "fluid_stack", "amount": 50, "fluid": "kubejs:" + dye }], "results": [{ "id": "minecraft:" + dye + "_terracotta" }] })
         e.custom({ "type": "create:filling", "ingredients": [{ "item": "create:framed_glass" }, { "type": "fluid_stack", "amount": 50, "fluid": "kubejs:" + dye }], "results": [{ "id": "createframed:" + dye + "_stained_framed_glass" }] })
+        e.custom({ "type": "create:filling", "ingredients": [{ "item": "create_d2d:asphalt" }, { "type": "fluid_stack", "amount": 50, "fluid": "kubejs:" + dye }], "results": [{ "id": "create_d2d:" + dye + "_asphalt" }] })
+        e.custom({ "type": "create:filling", "ingredients": [{ "item": "create_d2d:bore_block" }, { "type": "fluid_stack", "amount": 50, "fluid": "kubejs:" + dye }], "results": [{ "id": "create_d2d:" + dye + "_bore_block" }] })
     }
-
-
     var dye = ['red', 'orange', 'yellow', 'lime', 'green', 'cyan', 'light_blue', 'blue', 'purple', 'magenta', 'pink', 'white', 'light_gray', 'gray', 'black', 'brown']
     dye.forEach(D => { dyeCrafts(D) })
 })

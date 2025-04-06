29af071e7c62d8bb7155fc84c4edc4e810329fb2
MLDEG <mrmldeg@gmail.com>
2025-04-06 15:24:41 +0200

    Update 1.0.2

--- kubejs/server_scripts/crafts.js
+++ kubejs/server_scripts/crafts.js
@@ -3,4 +3,14 @@ ServerEvents.recipes(e => {
     //Plolished Rose Quartz Block
     e.shapeless('kubejs:polished_rose_quartz_block', '9x create:polished_rose_quartz')
     e.shapeless('9x create:polished_rose_quartz', 'kubejs:polished_rose_quartz_block')
+
+    //Coral Crafts
+    function corals(item) {
+        e.shapeless('4x minecraft:' + item + '_coral', 'minecraft:' + item + '_coral_block')
+        e.shapeless('minecraft:' + item + '_coral_block', '4x minecraft:' + item + '_coral')
+        e.shapeless('minecraft:' + item + '_coral', 'minecraft:' + item + '_coral_fan')
+        e.shapeless('minecraft:' + item + '_coral_fan', 'minecraft:' + item + '_coral')
+    }
+    var coral = ['horn', 'fire', 'bubble', 'brain', 'tube']
+    coral.forEach(I => { corals(I) })
 })
--- kubejs/server_scripts/create/mixing/crafts.js
+++ kubejs/server_scripts/create/mixing/crafts.js
@@ -3,6 +3,12 @@ ServerEvents.recipes(e => {
     //Tuff
     e.custom({ "type": "create:mixing", "heat_requirement": "heated", "ingredients": [{ "item": "minecraft:andesite" }, { "item": "minecraft:diorite" }, { "type": "fluid_tag", "amount": 250, "fluid_tag": "c:lava" }], "results": [{ "id": "minecraft:tuff" }] })
 
+    //Podzol
+    e.custom({ "type": "create:mixing", "ingredients": [{ "item": "minecraft:dirt" }, { "tag": "minecraft:leaves" }, { "type": "fluid_tag", "amount": 250, "fluid_tag": "c:water" }], "results": [{ "id": "minecraft:podzol" }] })
+
+    //Mycelium
+    e.custom({ "type": "create:mixing", "ingredients": [{ "item": "minecraft:dirt" }, { "tag": "c:mushrooms" }, { "type": "fluid_tag", "amount": 250, "fluid_tag": "c:water" }], "results": [{ "id": "minecraft:mycelium" }] })
+
     //Asphalt
     e.custom({ "type": "create:mixing", "heat_requirement": "heated", "ingredients": [{ "item": "minecraft:black_concrete_powder" }, { "type": "fluid_stack", "amount": 50, "fluid": "create_things_and_misc:slime" }], "results": [{ "id": "create_d2d:asphalt" }] })
 

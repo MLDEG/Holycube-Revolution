db3a8c5f00331fc4db541bf3309e2306bef5a67b
MLDEG <mrmldeg@gmail.com>
2025-05-03 17:20:35 +0200

    remove lootJS

--- kubejs/server_scripts/loot_tables.js
+++
@@ -1,7 +0,0 @@
-// LootJS.lootTables((e) => {
-//     e.create('minecraft:coarse_dirt').createPool((pool) => {
-//         pool.createPool()
-//         pool.addEntry('chipped:circle_oak_glass_pane')
-//     })
-// })
-

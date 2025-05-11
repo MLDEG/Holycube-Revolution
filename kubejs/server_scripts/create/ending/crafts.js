f793337519c38cad67fd9c552395087abc97a65b
MLDEG <mrmldeg@gmail.com>
2025-05-11 16:47:03 +0200

    1.0.8

---
+++ kubejs/server_scripts/create/ending/crafts.js
@@ -0,0 +1,4 @@
+ServerEvents.recipes(e => {
+    //Ender Pearl
+    e.custom({ "type": "create_dragons_plus:ending", "ingredients": [{ "item": "minecraft:slime_ball" }], "results": [{ "id": "minecraft:ender_pearl" }] })
+})
